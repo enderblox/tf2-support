dd455974536cd99184e7d3fb0185b65c720afb31
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-01-12 12:10:09 +0100

    push and own

---
+++ example-app/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>TF2 Support Site</title>
+
+        <link rel="stylesheet" href="resources/css/styles.css">
+    </head>
+    <body>
+
+    </body>
+</html>
